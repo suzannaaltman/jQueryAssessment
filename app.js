596c43ca947524f007e7f5767dd47c26da0f883a
suzannaaltman <suzannaaltman@gmail.com>
2016-04-01 15:53:18 -0500

    update counter

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var clicks = 1;
 
   $("#generate").on("click", function(event){
     $("body").append("<container id=\"moreButtons\"><br><button id=\"deleteButton\">Delete</button><button id=\"changeButton\">Change</button></container>");
+    $("body").append("<div id=\"counter\"></div>");
+    clickCounter();
   })
 
   $("body").on("click", "#deleteButton", function(event){  //delete function - also resets count
@@ -11,14 +13,12 @@ var clicks = 1;
   });
 
   $("body").on("click", "#changeButton", function(event){   //change function
-  $("#moreButtons").append("<div id=\"counter\"></div>");
   $(this).parent().toggleClass("red");
-    clickCounter();
   });
 
 function clickCounter(){
   $("#counter").empty();
-  $("#counter").append("<p id=\"counter\">Change button clicked " + clicks + " time(s).")
+  $("#counter").append("<p id=\"counter\">Generate button clicked " + clicks + " time(s).")
   clicks++;
 }
 
